d7bd1cd4a8e0e92908617434686eadcaf0b0ff6e
Vicente Llanos <vicente.d.llanos@gmail.com>
2023-01-24 13:07:14 -0300

    Agregado de la funcionalidad multilenguaje

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const flagsElement = document.getElementById("flags");
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) =>{
+  const requestJson = await fetch('languages/'+language+'.json');
+  const texts = await requestJson.json();
+
+  for (const textToChange of textsToChange){
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+
+    textToChange.innerHTML = texts[section][value];
+  }
+};
+
+flagsElement.addEventListener("click", (e) => {
+  console.log(e.target.parentElement.dataset.language);
+  changeLanguage(e.target.parentElement.dataset.language)
+});
